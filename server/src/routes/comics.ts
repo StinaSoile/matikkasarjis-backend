5d7ef2ad25e0bd7fa8d6acd70d38c0d627d22726
StinaSoile <vihri.gapoza@gmail.com>
2024-08-12 18:33:49 +0300

    feat: add error 404 to comicRouter - add error 404 if trying to post or get comic that does not exist

--- server/src/routes/comics.ts
+++ server/src/routes/comics.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import comicService from "../services/comicService";
+import { Comic } from "../types";
 const router = express.Router();
 
 // Postaa tietyn sarjakuvan tiettyyn sivuun liittyvät vastaukset tämän sivun osoitteeseen.
@@ -7,8 +8,17 @@ const router = express.Router();
 // Jos kyseessä on sivu 0 eli etusivu, palauttaa suoraan ensimmäisen tehtäväsivun keyn.
 // Jos vastaus on väärä, palauttaa keyn, joka vastaa sivua, jonka kysymyksiin tässä yritetään vastata.
 router.post("/:comicName/:page", (req, res) => {
-  const comic = comicService.getComic(req.params.comicName);
+  let comic: Comic;
+  try {
+    comic = comicService.getComic(req.params.comicName);
+  } catch (error: unknown) {
+    let errMsg = "Something went wrong.";
 
+    if (error instanceof Error) {
+      errMsg = error.message;
+    }
+    return res.status(404).send(errMsg);
+  }
   try {
     const key = comicService.returnKeyByAnswer(
       req.body,
@@ -29,8 +39,17 @@ router.post("/:comicName/:page", (req, res) => {
 // Palauttaa sarjakuvan kaikki sivut jotka saa sillä avaimella, joka on getin mukana queryssa
 // Listan viimeinen, ts se jonka key on annettu, palautetaan ilman tietoja oikeista vastauksista.
 router.get("/:comicName", (req, res) => {
-  const comic = comicService.getComic(req.params.comicName);
+  let comic: Comic;
+  try {
+    comic = comicService.getComic(req.params.comicName);
+  } catch (error: unknown) {
+    let errMsg = "Something went wrong.";
 
+    if (error instanceof Error) {
+      errMsg = error.message;
+    }
+    return res.status(404).send(errMsg);
+  }
   const key = req.query.key as string | undefined;
   try {
     const pagesToReturn = comicService.getPagesToReturn(key, comic.comicpages);
@@ -48,8 +67,17 @@ router.get("/:comicName", (req, res) => {
 
 // yksittäiset sivut kysymyksineen ja vastauksineen, rajoitettu keyn perusteella
 router.get("/:comicName/:page", (req, res) => {
-  const comic = comicService.getComic(req.params.comicName);
+  let comic: Comic;
+  try {
+    comic = comicService.getComic(req.params.comicName);
+  } catch (error: unknown) {
+    let errMsg = "Something went wrong.";
 
+    if (error instanceof Error) {
+      errMsg = error.message;
+    }
+    return res.status(404).send(errMsg);
+  }
   const key = req.query.key as string | undefined;
   try {
     const pagesToReturn = comicService.getPagesToReturn(key, comic.comicpages);
