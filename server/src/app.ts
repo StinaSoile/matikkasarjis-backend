bf609e75df95710edf0fb001b277f47e20b1c3f8
StinaSoile <vihri.gapoza@gmail.com>
2024-08-12 14:05:42 +0300

    feat: remove unused code from app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,4 @@
 import express, { Application } from "express";
-import path from "path";
-import fs from "fs";
 import imageRouter from "./routes/images";
 import comicRouter from "./routes/comics";
 
@@ -17,19 +15,3 @@ app.use(cors());
 app.use("/api/images", imageRouter);
 
 app.use("/api/comics", comicRouter);
-
-// saadaan lista kaikkien kuvien urleja.
-// En tiedä mitä tällä tehdään, mutta toistaiseksi on tällainen jos tarvitsee.
-app.get("/api/imagelist", (_req, res) => {
-  const imageListPath = path.join(
-    __dirname,
-    "../../public/generatedImageUrls.json"
-  );
-  fs.readFile(imageListPath, "utf8", (err, data) => {
-    if (err) {
-      res.status(500).json({ error: "Failed to read image list" });
-      return;
-    }
-    res.json(JSON.parse(data));
-  });
-});
