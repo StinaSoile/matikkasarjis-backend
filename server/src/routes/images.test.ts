3d2433005e0ed8db23748822bb336beaaaa8a8f7
StinaSoile <vihri.gapoza@gmail.com>
2024-08-12 14:06:36 +0300

    test: add tests for images.ts and utils.ts - finish testing all parts of images.ts - add one missing test to utils.ts

--- server/src/routes/images.test.ts
+++ server/src/routes/images.test.ts
@@ -1,26 +1,47 @@
 import supertest from "supertest";
 import test from "node:test";
 import { app } from "../app";
-// import assert from "node:assert";
+import assert from "node:assert";
 
 const api = supertest(app);
 
 test.describe("testing get(`api/images/:comicName/:imageName`)", () => {
   test("should return image if it exists in the path", async () => {
-    await api.get("/api/images/siivetonlepakko/lepakko-s1.png").expect(200);
+    const kuva1 = await api
+      .get("/api/images/siivetonlepakko/lepakko-s1.png")
+      .expect(200);
+    assert.deepEqual(kuva1.headers["content-type"], "image/png");
+
+    const kuva2 = await api
+      .get("/api/images/velhontaloudenhoitaja/velhontaloudenhoitaja-s4.png")
+      .expect(200);
+    assert.deepEqual(kuva2.headers["content-type"], "image/png");
   });
 
-  //   test("if no key, throws specific error", async () => {
-  //     const result = await api.get("/api/comics/siivetonlepakko/2").expect(400);
+  test("should throw error if path or image name is wrong", async () => {
+    const result1 = await api
+      .get("/api/images/siivetonrapakko/lepakko-s1.png")
+      .expect(404);
+    assert.strictEqual(result1.text, "Image not found");
 
-  //     assert.strictEqual(result.text, "Key is required");
-  //   });
+    const result2 = await api
+      .get("/api/images/siivetonlepakko/s1.png")
+      .expect(404);
+    assert.strictEqual(result2.text, "Image not found");
+  });
+});
 
-  //   test("if key is for earlier pages, throws specific error", async () => {
-  //     const result = await api
-  //       .get("/api/comics/siivetonlepakko/5?key=eka")
-  //       .expect(400);
+test.describe("testing get(`api/images/:imageName`)", () => {
+  test("should return image if it exists in the path", async () => {
+    const kuva1 = await api.get("/api/images/etusivunkuva.png").expect(200);
+    assert.deepEqual(kuva1.headers["content-type"], "image/png");
+  });
 
-  //     assert.strictEqual(result.text, "Index out of bounds");
-  //   });
+  test("should throw error if path or image name is wrong", async () => {
+    const result1 = await api.get("/api/images/ei").expect(404);
+    assert.strictEqual(result1.text, "Image not found");
+
+    const result2 = await api.get("/api/images/s1.png").expect(404);
+    assert.strictEqual(result2.text, "Image not found");
+  });
 });
