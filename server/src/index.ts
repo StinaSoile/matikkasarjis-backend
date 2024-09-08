a65e9a64807932eac32ef95fa5dc17f0f5de4040
StinaSoile <vihri.gapoza@gmail.com>
2024-09-08 19:46:40 +0300

    fix: try to fix process.env.MONGODB_URI

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import { app } from "./app";
-import config from "./utils/config";
+// import config from "./utils/config";
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
 const PORT = 3333;
 
-const url = config.MONGODB_URI as string;
+const url = process.env.MONGODB_URI as string;
 
 console.log("connecting to MongoDB");
 mongoose
